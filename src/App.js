521c3e13caf506c619f4fc8effbe29464f555ee5
SIDDHARTH GORE <siddharth.gore@live.com>
2024-07-01 14:50:43 +0530

    Complete App Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [count, setCount] = useState(0)
+
+
+  const handleIncrement = () => {
+
+    setCount(count + 1)
+    console.log('handleIncrement')
+  }
+
+  const handleDecrement = () => {
+
+
+
+    setCount(count - 1)
+    console.log('handleDecrement')
+  }
+
+  const handleReset = () => {
+
+
+    setCount(0)
+    console.log('handleReset')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container' >
+
+      <h1 className='title'>Counter App</h1>
+
+      <div className='counter-display'>{count}</div>
+
+      <div className='button-container'>
+        <button className='button increment' onClick={handleIncrement}>Increment</button>
+        <button className='button decrement' onClick={handleDecrement}>Decrement</button>
+        <button className='button reset' onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
